11197ae6f34115595fb7306979e85ffc28109970
André Venâncio <andreven2002@gmail.com>
2018-12-17 19:11:40 +0000

    All app components. Getting the handwriting styles, still not generating the final image.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AvaliationProject'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('AvaliationProject');
-  });
+  // it(`should have as title 'AvaliationProject'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('AvaliationProject');
+  // });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to AvaliationProject!');
-  });
+  // it('should render title in a h1 tag', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to AvaliationProject!');
+  // });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { ApiRequestsService } from './api-requests.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+export interface HandwritingStylesItem { //Data model from the list of handwriting styles items obtained from the handwriting.io API
+  id: string;
+  title: string;
+  date_created: Date;
+  date_modified: Date;
+  rating_neatness: number;
+  rating_cursivity: number;
+  rating_embellishment: number;
+  rating_character_width: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'AvaliationProject';
+  handwritingStylesList: Array<HandwritingStylesItem>; //List of handwriting styles
+  handwritingOptions: FormGroup; //Form group, used to do the validator
+
+  constructor(private request: ApiRequestsService, private formBuilder: FormBuilder) {
+    this.request.getHandwritings().subscribe((data: Array<HandwritingStylesItem>) => { //API request to get handwriting styles list
+      this.handwritingStylesList = data;
+    });
+  }
+
+  ngOnInit() {
+    this.handwritingOptions = this.formBuilder.group({
+      handwriting: ['', [Validators.required]],
+      fontSize: [20, [Validators.required, Validators.min(0), Validators.max(9000)]],
+      color:['#000000', [Validators.required]],
+      text:['', [Validators.required]]
+    });
+  }
 }
